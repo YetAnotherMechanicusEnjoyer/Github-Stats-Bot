2bbb64f1c0d63b67ee3e296608368945d0e28421
YetAnotherMechanicusEnjoyer <ahmed.al-rawi@epitech.eu>
2025-07-30 00:02:48 +0200

    [UPDATE] index.cjs
    [ADD] loadThemes()

--- index.cjs
+++ index.cjs
@@ -3,12 +3,14 @@ const intents = new Discord.IntentsBitField(3276799);
 const bot = new Discord.Client({ intents });
 const loadCommands = require("./src/loaders/loadCommands.cjs");
 const loadEvents = require("./src/loaders/loadEvents.cjs");
+const loadThemes = require("./src/loaders/loadThemes.cjs");
 require('dotenv').config();
 
 bot.commands = new Discord.Collection();
-bot.profiles = new Discord.Collection();
+bot.themes = new Discord.Collection();
 bot.color = 0x1ec1e6;
 bot.login(process.env.TOKEN);
 
+loadThemes(bot);
 loadCommands(bot);
 loadEvents(bot);
