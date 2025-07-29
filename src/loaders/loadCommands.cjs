accc99e78afa8e0065e3f982c2163dddfd71e749
YetAnotherMechanicusEnjoyer <ahmed.al-rawi@epitech.eu>
2025-07-30 00:02:33 +0200

    [ADD] src/loaders/loadThemes.cjs

--- src/loaders/loadCommands.cjs
+++ src/loaders/loadCommands.cjs
@@ -2,12 +2,12 @@ const fs = require("fs")
 
 module.exports = async bot => {
 
-  console.log("\n[⏳]\x1b[33mCommands Loading...\x1b[0m");
+  console.log("[⏳]\x1b[33mCommands Loading...\x1b[0m");
   fs.readdirSync("./src/cmds").filter(f => f.endsWith(".mjs")).forEach(async file => {
 
     let mod = require(`../cmds/${file}`);
     let command = mod.command;
-    if (!command.name || typeof command.name !== "string") throw new TypeError(`La commande ${file.slice(0, file.length - 4)} n'a pas de nom !`)
+    if (!command.name || typeof command.name !== "string") throw new TypeError(`Command ${file.slice(0, file.length - 4)} doesn't have a name !`)
     bot.commands.set(command.name, command)
     console.log(`   [✅]\x1b[32m${file.slice(0, file.length - 4)}\x1b[0m`);
   })
---
+++ src/loaders/loadThemes.cjs
@@ -0,0 +1,9 @@
+module.exports = async bot => {
+  console.log("\n[⏳]\x1b[33mThemes Loading...\x1b[0m");
+  const themes = require("../assets/themes.mjs").themes;
+  themes.forEach(theme => {
+    if (typeof theme !== "string") throw new TypeError(`Theme ${theme} doesn't have a name !`)
+    bot.themes.set(theme, theme);
+    console.log(`   [✅]\x1b[32m${theme}\x1b[0m`);
+  });
+}
