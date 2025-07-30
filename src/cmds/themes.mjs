dbf880bd2e78d354db7526324754284352b53906
YetAnotherMechanicusEnjoyer <ahmed.al-rawi@epitech.eu>
2025-07-30 19:30:31 +0200

    [ADD] src/cmds/themes.mjs

---
+++ src/cmds/themes.mjs
@@ -0,0 +1,38 @@
+export const command = {
+  name: "themes",
+  description: "Displays all available cards themes.",
+  permission: "None",
+  context: "None",
+  category: "Github Stats",
+  options: [],
+
+  async run(bot, interaction) {
+    await interaction.deferReply();
+
+    let themes_str = "";
+
+    bot.themes.forEach(theme => {
+      themes_str += "\n" + theme;
+    });
+
+    let Embed = {
+      color: bot.color,
+      title: `Available cards themes`,
+      author: {
+        name: interaction.member.user.username,
+        iconURL: interaction.member.user.displayAvatarURL({ dynamic: true }),
+      },
+      description: themes_str,
+      thumbnail: {
+        url: bot.user.displayAvatarURL({ dynamic: true }),
+      },
+      timestamp: new Date().toISOString(),
+      footer: {
+        text: `/themes`,
+        iconURL: bot.user.displayAvatarURL({ dynamic: true }),
+      },
+    };
+
+    await interaction.editReply({ embeds: [Embed] });
+  }
+}
